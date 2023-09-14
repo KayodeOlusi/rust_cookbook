cb8f9c27f09b91264a4209375ffa74c1536e4910
KayodeOlusi <kayodeolusi@gmail.com>
2023-09-14 18:48:08 +0100

    assignments and boolean in rust

---
+++ chapter 1/assignment.rs
@@ -0,0 +1,25 @@
+use std::io::stdin;
+use std::{f32, f64, i32, i64, i8, isize, u16, u32, u64, u8, usize};
+
+// fn main() {
+//     println!("Understanding assignments");
+
+//     // Compiler will automatically figure out the data type if not mentioned
+//     // Cannot change the value
+//     let num = 10;
+//     println!("Num is {}", num);
+// }
+
+// fn main() {
+//     let age: i32 = 40;
+//     println!("Age is {}", age);
+
+//     // Prints max and min value of the 32bits integer
+//     println!("Max i32 {}", i32::MAX);
+//     println!("Min i32 {}", i32::MIN);
+// }
+
+fn main() {
+    let (f_name, l_name) = ("viki", "d");
+    println!("First name is {0} and last name is {1}", f_name, l_name);
+}
